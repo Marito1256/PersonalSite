c51e4917a98ce016b99be909a7e4c824c45da7d6
Marito1256 <maritoespana@gmail.com>
2025-03-22 04:40:18 -0700

    adding the start script used on servers

--- startSite.sh
+++ startSite.sh
@@ -1,5 +1,5 @@
-docker stop CairParavel
-docker rm CairParavel
+docker stop PersonalSite
+docker rm PersonalSite
 docker rmi marito1256/cairparavel-webserver
 docker pull marito1256/cairparavel-webserver:latest
-docker run --name CairParavel -d -p 5000:5000 marito1256/cairparavel-webserver:latest
+docker run --name PersonalSite -d -p 5000:5000 marito1256/cairparavel-webserver:latest
