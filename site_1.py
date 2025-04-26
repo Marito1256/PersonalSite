d7fec07fc27992232149ef5ac0bc0b723bd213f1
[Marito <maritoespana@gmail.com>
2025-04-26 05:24:10 -0700

    added Postgres db connection, can start working on API now

--- site_1.py
+++ site_1.py
@@ -1,8 +1,34 @@
 from flask import Flask, render_template
+import psycopg2
+import os
+from dotenv import load_dotenv
 #from werkzeug.middleware.proxy_fix import ProxyFix
+
+#Loading environment variables from the env file (create this locally with the correct credentials)
+load_dotenv()
 
 app = Flask(__name__, static_folder="static")
 
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+
+SQLconnection = psycopg2.connect(
+    dbname = DB_NAME,
+    user = DB_USER,
+    password = DB_PASSWORD,
+    # host = DB_HOST,
+    host = '192.168.50.136',
+    port = DB_PORT
+)
+#test connection
+
+cursor1 = SQLconnection.cursor()
+cursor1.execute('SELECT version();')
+db_version = cursor1.fetchone()
+print(f"Connected to: {db_version}")
 #adding https support behind a reverse proxy
 #app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
